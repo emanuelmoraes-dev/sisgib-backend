cc9529ab3d7a8052805c665469ca9cf4dd26e86a
Emanuel Moraes <emanuelmoraes297@gmail.com>
2019-09-29 21:36:45 -0300

    regras de cadastro e edição de emprestimos

--- src/models/emprestimo.js
+++ src/models/emprestimo.js
@@ -1,3 +1,4 @@
+const { plus, PERIODS } = require('datetime-utility')
 const { Entity } = require('alpha-restful')
 const restful = require('../global/restful')
 const mongoose = require('mongoose')
@@ -6,7 +7,7 @@ const ObjectId = mongoose.Schema.Types.ObjectId
 const Emprestimo = new Entity({
 	name: 'Emprestimo',
 	resource: 'emprestimos',
-	methods: ['get'],
+	methods: ['get', 'post', 'patch'],
 	descriptor: {
 		dataEmprestimo: { type: Date, required: true },
 		dataDevolucao: { type: Date, required: true },
@@ -29,6 +30,29 @@ const Emprestimo = new Entity({
 		livro: 'Livro'
 	}
 })
+
+Emprestimo.beforeCreate = async (emprestimo, req, res, next) => {
+	const { Livro } = restful.entities
+
+	emprestimo.dataEmprestimo = new Date()
+	emprestimo.dataDevolucao = plus(emprestimo.dataEmprestimo, PERIODS.DAY, req.body.livro.duracaoEmprestimo)
+	emprestimo.ativo = true
+
+	await Livro.model.updateOne({ _id: emprestimo.livro.id }, { $set: { reservado: true } })
+	next()
+}
+
+Emprestimo.beforeEdit = async (emprestimo, req, res, next) => {
+	const { Livro } = restful.entities
+	const salved = await restful.query({ _id: req.body.id }, Emprestimo, { findOne: true })
+
+	if (!emprestimo.ativo && salved.ativo) {
+		await Livro.model.updateOne({ _id: emprestimo.livro.id }, { $set: { reservado: false } })
+		emprestimo.dataDevolucao = new Date()
+	}
+
+	next()
+}
 
 restful.add(Emprestimo)
 
